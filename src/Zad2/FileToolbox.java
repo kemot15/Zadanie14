38b1292343db16d30367ae2257f66ec7623de3de
Tomek <kemot15@tlen.pl>
2019-08-20 20:53:48 +0200

    zad 2 ver 1 metoda fileCreator w jakims dziwnym formacie zapisuje plik - lepiej nie uzywac:)

--- src/Zad2/FileToolbox.java
+++ src/Zad2/FileToolbox.java
@@ -1,14 +1,14 @@
 package Zad2;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class FileToolbox {
     public static void fileCreator (String path, int qtyNumbers) throws IOException {
-        Random random = new Random(100);
+        Random random = new Random();
         File file = new File(path);
         if (!file.exists()){
             file.createNewFile();
@@ -16,8 +16,27 @@ public class FileToolbox {
         FileWriter fr = new FileWriter(file);
         BufferedWriter bw = new BufferedWriter(fr);
         for (int i = 0; i < qtyNumbers; i++){
-            bw.write(random.nextInt());
+            int rand = random.nextInt(100);
+            bw.write(rand);
+            bw.newLine();
         }
         bw.close();
+    }
+
+    public static List<Integer> fileReader (String path) throws IOException {
+        List<Integer> numbers = new ArrayList<>();
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextInt()){
+           numbers.add(scanner.nextInt());
+        }
+        //FileReader fr = new FileReader(file);
+//        BufferedReader br = new BufferedReader(fr);
+//        String line = null;
+//        while ((line = br.readLine()) != null){
+//            numbers.add(Integer.valueOf(line));
+//            System.out.println(Integer.valueOf(line));
+//        }
+        return numbers;
     }
 }
--- src/Zad2/Main.java
+++ src/Zad2/Main.java
@@ -1,14 +1,34 @@
 package Zad2;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "liczby.txt";
+        String path = "Liczby.txt";
         try {
-            FileToolbox.fileCreator(path, 25);
+           // FileToolbox.fileCreator(path, 25);
+            List<Integer> numbers = FileToolbox.fileReader(path);
+            System.out.println("Liczby w pliku: "+ FileToolbox.fileReader(path));
+            showResults(numbers);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    private static void showResults (List<Integer> numbers){
+        int[] newList = new int[Collections.max(numbers)+1];
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for (int i = 0; i < numbers.size(); i++){
+            newList[numbers.get(i)]++;
+            treeSet.add(numbers.get(i));
+        }
+        for (Integer num : treeSet){
+            System.out.println(num + " - liczba wystapien: " + newList[num]);
         }
     }
 }
